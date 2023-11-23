f79fae9bcf497e3d451c501354b09d7884cfef1e
IvanAbramovich <Ivan.abr.working@gmail.com>
2023-11-23 15:12:19 +0700

    fixed tests

--- hw_11/keylist_test.erl
+++ hw_11/keylist_test.erl
@@ -8,17 +8,13 @@ keylist_test_() ->
     setup,
     fun start/0,
     fun stop/1,
-    fun(SetupData) ->
-      [
-        is_registered_test(SetupData),
-        adding_test(SetupData),
-        is_member_test(SetupData),
-        take_test(SetupData),
-        find_test(SetupData),
-        delete_test(SetupData),
-        stop_test(SetupData)
-      ]
-    end
+    [
+      fun adding_test/0,
+      fun is_member_test/0,
+      fun find_test/0,
+      fun delete_test/0,
+      fun stop_test/0
+    ]
   }.
 
 start() ->
@@ -30,24 +26,19 @@ stop(Pid) ->
   ?debugFmt("stopped", []),
   keylist:stop(Pid).
 
-is_registered_test(Pid) ->
-  ?_assertEqual(Pid, whereis(keylist1)).
 
-adding_test(Pid) ->
-  ?_assertMatch({ok, _Counter}, keylist:add(Pid, a, 3, "ab")).
+adding_test() ->
+  ?_assertMatch({ok, _Counter}, keylist:add(keylist1, a, 3, "ab")).
 
-is_member_test(Pid) ->
-  ?_assertEqual({ok, true}, keylist:is_member(Pid, a)).
+is_member_test() ->
+  ?_assertEqual({ok, true}, keylist:is_member(keylist1, a)).
 
-take_test(Pid) ->
-  keylist:add(Pid, a, 3, "ab"),
-  ?_assertMatch({ok, _Item}, keylist:take(Pid, a)).
 
-find_test(Pid) ->
-  ?_assertMatch({ok, _Item}, keylist:find(Pid, a)).
+find_test() ->
+  ?_assertMatch({ok, _Item}, keylist:find(keylist1, a)).
 
-delete_test(Pid) -> 
-  ?_assertMatch({ok, _Counter}, keylist:delete(Pid, a)).
+delete_test() -> 
+  ?_assertMatch({ok, _Counter}, keylist:delete(keylist1, a)).
 
-stop_test(_Pid) ->
+stop_test() ->
   ?_assertEqual(ok, keylist:stop(keylist1)).
